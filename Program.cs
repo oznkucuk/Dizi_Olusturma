e3ea40b9bccd53c40240308dd3bfdb579c8122aa
oznkucuk <oznkucuk@gmail.com>
2023-02-16 02:09:18 +0300

    Dizi_Olusturma
    C# Dizi Calismalari

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace dizi_ornekleri1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KULLANICIDAN DİZİ ELEMANLARINI AL VE EKRANA YAZDIR");
+            int[] sayilar = new int[4];
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write("Sayılar dizisinin "+i+". elemanını giriniz: ");
+                sayilar[i]=Convert.ToInt32(Console.ReadLine());             
+            }
+            Console.WriteLine("Dizinin elemanlarını belirlediniz şimdi elemanları yazdırıyoruz..");
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine(sayilar[i]);
+            }
+
+
+        }
+    }
+}
